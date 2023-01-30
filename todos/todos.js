9d649aeac8090e5b8307d2e8d14d8c90d9c6d834
Jake Morgan <jakecmorgan.2000@gmail.com>
2023-01-30 15:14:06 -0800

    completed deleteAllTodos() in fetch-utils.js

--- todos/todos.js
+++ todos/todos.js
@@ -69,5 +69,7 @@ logoutButton.addEventListener('click', () => {
 
 deleteButton.addEventListener('click', async () => {
     // delete all todos
+    await deleteAllTodos();
     // then refetch and display the updated list of todos
+    displayTodos();
 });
